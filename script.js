5fa562c970e3822e66c9c3e6f1aa4a3dc8308b9b
Tward9 <97201650+Tward9@users.noreply.github.com>
2022-02-14 11:53:05 -0600

    set up length selection, validate length option

--- script.js
+++ script.js
@@ -8,6 +8,40 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //create generatePassword function
+  var generatePassword = function () {
+    //prompts set up password criteria
+    alert("Please input Password Criteria.");
+    //user responds to prompts, responses stored to select password criteria
+    //first choice, password length 8 characters up to and including 128 characters
+    var lengthChoice = prompt("Select Password Length, minimum 8 characters, maximum 128 characters");
+    //convert any strings to numbers 
+    //validate choice one selection
+    while (lengthChoice < 8 || lengthChoice > 128) {
+      if (lengthChoice >= 8 && lengthChoice <= 128) {
+        break;
+      }
+      alert("Password length selection must be between 8 and 128 characters");
+      lengthChoice = prompt("Select Password Length, between 8 and 128 characters");
+      //convert any string to numbers
+    }
+  }
+
+  //choice two, lowercase
+  //validate choice two selection
+
+  //choice three, uppercase
+  //validate choice three selection
+
+  //choice four, numbers
+  //validate choice four selection
+
+  //choice five, special characters: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+  //validate choice five selection
+
+  //generate random password based off of selected criteria
+
+  //display password in box or alert
 }
 
 // Add event listener to generate button
@@ -15,25 +49,4 @@ generateBtn.addEventListener("click", writePassword);
 
 //clicking button presents several propmpts to user
 
-//prompts set up password criteria
 
-//user responds to prompts, responses stored to select password criteria
-
-//first choice, password length 8 characters up to and including 128 characters
-//validate choice one selection
-
-//choice two, lowercase
-//validate choice two selection
-
-//choice three, uppercase
-//validate choice three selection
-
-//choice four, numbers
-//validate choice four selection
-
-//choice five, special characters: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-//validate choice five selection
-
-//generate random password based off of selected criteria
-
-//display password in box or alert
