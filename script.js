b8b136bdf24d2e96b0006af5b7bf16b825bc3333
Tward9 <97201650+Tward9@users.noreply.github.com>
2022-02-14 11:34:49 -0600

    psuedo code javascript steps

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//clicking button presents several propmpts to user
+
+//prompts set up password criteria
+
+//user responds to prompts, responses stored to select password criteria
+
+//first choice, password length 8 characters up to and including 128 characters
+//validate choice one selection
+
+//choice two, lowercase
+//validate choice two selection
+
+//choice three, uppercase
+//validate choice three selection
+
+//choice four, numbers
+//validate choice four selection
+
+//choice five, special characters: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+//validate choice five selection
+
+//generate random password based off of selected criteria
+
+//display password in box or alert
