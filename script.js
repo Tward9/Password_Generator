94e8c1fb918a4558952f676f6e025c54d4ec6320
Tward9 <97201650+Tward9@users.noreply.github.com>
2022-02-16 15:56:46 -0600

    select special char, rerun criteria on all no

--- script.js
+++ script.js
@@ -18,96 +18,121 @@ generateBtn.addEventListener("click", writePassword);
 
 //create generatePassword function
 function generatePassword() {
-  //prompts set up password criteria
-  var proceed = confirm("Do you want to generate a Password");
-  console.log(proceed);
-  if (proceed == false) {
-    return;
-  }
-  alert("Please input Password Criteria.");
-  //user responds to prompts, responses stored to select password criteria
-  //first choice, password length 8 characters up to and including 128 characters
-  var lengthChoice = prompt("Select Password Length, minimum 8 characters, maximum 128 characters");
-  //convert any strings to numbers 
-  //validate choice one selection
-  while (lengthChoice < 8 || lengthChoice > 128) {
-    if (lengthChoice >= 8 && lengthChoice <= 128) {
-      break;
+  function passwordCriteria(){
+    //prompts set up password criteria
+    var proceed = confirm("Do you want to generate a Password");
+    console.log(proceed);
+    if (proceed == false) {
+      return;
     }
-    alert("Password length selection must be between 8 and 128 characters");
-    lengthChoice = prompt("Select Password Length, between 8 and 128 characters");
-    //convert any string to numbers
-  }
-  console.log(lengthChoice);
-  //choice two, lowercase
-  var lowerCase = prompt("Do you want Lowercase Letters? (Y)es, (N)o");
-  lowerCase = lowerCase.toLowerCase();
-  //validate choice and convert anything to boolean
-  while (lowerCase != "yes" || lowerCase != "no") {
-    if (lowerCase == "yes" || lowerCase == "no") {
-      break;
-    } else if (lowerCase == "y" || lowerCase == "n") {
-      break;
+    alert("Please input Password Criteria.");
+    //user responds to prompts, responses stored to select password criteria
+    //first choice, password length 8 characters up to and including 128 characters
+    var lengthChoice = prompt("Select Password Length, minimum 8 characters, maximum 128 characters");
+    //convert any strings to numbers 
+    //validate choice one selection
+    while (lengthChoice < 8 || lengthChoice > 128) {
+      if (lengthChoice >= 8 && lengthChoice <= 128) {
+        break;
+      }
+      alert("Password length selection must be between 8 and 128 characters");
+      lengthChoice = prompt("Select Password Length, between 8 and 128 characters");
+      //convert any string to numbers
     }
-    alert("Must choose (Y)es or (N)o");
-    lowerCase = prompt("Do you want Lowercase Letters? (Y)es, (N)o");
+    console.log(lengthChoice);
+    //choice two, lowercase
+    var lowerCase = prompt("Do you want Lowercase Letters? (Y)es, (N)o");
     lowerCase = lowerCase.toLowerCase();
-  }
-  //convert to boolean
-  if (lowerCase == "y" || lowerCase == "yes") {
-    lowerCase = true;
-  } else {
-    lowerCase = false;
-  }
-  console.log(lowerCase);
-
-  //choice three, uppercase
-  var upperCase = prompt("Do you want Uppercase Letters? (Y)es, (N)o");
-  upperCase = upperCase.toLowerCase();
-  //validate choice and convert anything to boolean
-  while (upperCase != "yes" || upperCase != "no") {
-    if (upperCase == "yes" || upperCase == "no") {
-      break;
-    } else if (upperCase == "y" || upperCase == "n") {
-      break;
+    //validate choice and convert anything to boolean
+    while (lowerCase != "yes" || lowerCase != "no") {
+      if (lowerCase == "yes" || lowerCase == "no") {
+        break;
+      } else if (lowerCase == "y" || lowerCase == "n") {
+        break;
+      }
+      alert("Must choose (Y)es or (N)o");
+      lowerCase = prompt("Do you want Lowercase Letters? (Y)es, (N)o");
+      lowerCase = lowerCase.toLowerCase();
     }
-    alert("Must choose (Y)es or (N)o");
-    upperCase = prompt("Do you want Uppercase Letters? (Y)es, (N)o");
+    //convert to boolean
+    if (lowerCase == "y" || lowerCase == "yes") {
+      lowerCase = true;
+    } else {
+      lowerCase = false;
+    }
+    console.log(lowerCase);
+
+    //choice three, uppercase
+    var upperCase = prompt("Do you want Uppercase Letters? (Y)es, (N)o");
     upperCase = upperCase.toLowerCase();
-  }
-  //convert to boolean
-  if (upperCase == "y" || upperCase == "yes") {
-    upperCase = true;
-  } else {
-    upperCase = false;
-  }
-  console.log(upperCase);
-
-  //choice four, numbers
-  var numb = prompt("Do you want Numbers? (Y)es, (N)o");
-  numb = numb.toLowerCase();
-  //validate choice and convert anything to boolean
-  while (numb != "yes" || numb != "no") {
-    if (numb == "yes" || numb == "no") {
-      break;
-    } else if (numb == "y" || numb == "n") {
-      break;
+    //validate choice and convert anything to boolean
+    while (upperCase != "yes" || upperCase != "no") {
+      if (upperCase == "yes" || upperCase == "no") {
+        break;
+      } else if (upperCase == "y" || upperCase == "n") {
+        break;
+      }
+      alert("Must choose (Y)es or (N)o");
+      upperCase = prompt("Do you want Uppercase Letters? (Y)es, (N)o");
+      upperCase = upperCase.toLowerCase();
     }
-    alert("Must choose (Y)es or (N)o");
-    numb = prompt("Do you want Numbers? (Y)es, (N)o");
+    //convert to boolean
+    if (upperCase == "y" || upperCase == "yes") {
+      upperCase = true;
+    } else {
+      upperCase = false;
+    }
+    console.log(upperCase);
+
+    //choice four, numbers
+    var numb = prompt("Do you want Numbers? (Y)es, (N)o");
     numb = numb.toLowerCase();
-  }
-  //convert to boolean
-  if (numb == "y" || numb == "yes") {
-    numb = true;
-  } else {
-    numb = false;
-  }
-  console.log(numb);
+    //validate choice and convert anything to boolean
+    while (numb != "yes" || numb != "no") {
+      if (numb == "yes" || numb == "no") {
+        break;
+      } else if (numb == "y" || numb == "n") {
+        break;
+      }
+      alert("Must choose (Y)es or (N)o");
+      numb = prompt("Do you want Numbers? (Y)es, (N)o");
+      numb = numb.toLowerCase();
+    }
+    //convert to boolean
+    if (numb == "y" || numb == "yes") {
+      numb = true;
+    } else {
+      numb = false;
+    }
+    console.log(numb);
 
-  //choice five, special characters: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    //choice five, special characters: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    var specialchar = prompt("Do you want Special Characters? (Y)es, (N)o");
+    specialchar = specialchar.toLowerCase();
+    //validate choice and convert anything to boolean
+    while (specialchar != "yes" || specialchar != "no") {
+      if (specialchar == "yes" || specialchar == "no") {
+        break;
+      } else if (specialchar == "y" || specialchar == "n") {
+        break;
+      }
+      alert("Must choose (Y)es or (N)o");
+      specialchar = prompt("Do you want Special Characters? (Y)es, (N)o");
+      specialchar = specialchar.toLowerCase();
+    }
+    //convert to boolean
+    if (specialchar == "y" || specialchar == "yes") {
+      specialchar = true;
+    } else {
+      specialchar = false;
+    }
+    console.log(specialchar);
+  }
 
-  //validate choice five selection
+  if (numb == false && specialchar == false && upperCase == false && lowerCase == false) {
+    alert("You have at least one selection")
+    passwordCriteria();
+  }
   //reask questions if all answers are no
   //arays for choices to pull from for final password
   //numbers, letters upper, letters lower, spectial characters
